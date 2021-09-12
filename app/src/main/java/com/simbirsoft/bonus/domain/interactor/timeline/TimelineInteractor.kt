ac2e61d3cbeee4fc220d937f377ad1447c6e7c88
dmitry <dmitrypokrasov@gmail.com>
2021-09-12 13:21:29 +0400

    refactoring

--- app/src/main/java/com/simbirsoft/bonus/domain/interactor/timeline/TimelineInteractor.kt
+++ app/src/main/java/com/simbirsoft/bonus/domain/interactor/timeline/TimelineInteractor.kt
@@ -3,5 +3,5 @@ package com.simbirsoft.bonus.domain.interactor.timeline
 import com.simbirsoft.bonus.domain.entity.timeline.TimeLineScreenModel
 
 interface TimelineInteractor {
-    suspend fun getTimeLineScreenModels(): TimeLineScreenModel
+    suspend fun getTimeLineScreenModels(): TimeLineScreenModel?
 }
